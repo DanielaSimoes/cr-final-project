8aea6cf013acf856bc1845bf04f0663a410d0d02
Daniela Simões <danielasimoes@ua.pt>
2019-05-26 18:19:33 +0100

    init.

---
+++ final_project/final_project.srcs/sources_1/new/TopLevel.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity TopLevel is
+  Port ( clk : in STD_LOGIC );
+end TopLevel;
+
+architecture Behavioral of TopLevel is
+
+begin
+
+
+end Behavioral;
